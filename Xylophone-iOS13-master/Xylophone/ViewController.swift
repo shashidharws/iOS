5adb7a232af82cf839a426c4ce54aa696e3cb31d
schandrappa <shashidhar.chandrappa@commscope.com>
2019-10-22 14:05:43 +0530

    Final Xylophone with challenge

--- Xylophone-iOS13-master/Xylophone/ViewController.swift
+++ Xylophone-iOS13-master/Xylophone/ViewController.swift
@@ -7,15 +7,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        //print(String(sender.titleLabel!.text!))
+        print(sender.currentTitle!)
+        sender.alpha = 0.5
+        
+        print("Start")
+       
+        let seconds = 0.2
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            // Put your code which should be executed with a delay here
+            sender.alpha = 1.0
+            print("End")
+        }
+        playSound(soundTrack: sender.currentTitle!)
+    }
     
-
+    func playSound(soundTrack: String) {
+        let url = Bundle.main.url(forResource: soundTrack, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
-
